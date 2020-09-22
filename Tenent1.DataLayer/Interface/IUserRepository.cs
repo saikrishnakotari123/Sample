825635717018f3cba0ea8c85b817a35f065d4a5b
saikrishnakotari123 <71275733+saikrishnakotari123@users.noreply.github.com>
2020-09-22 18:34:19 +0530

    Add files via upload

---
+++ Tenent1.DataLayer/Interface/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tenent1.BusniessLayer.Entites;
+
+namespace Tenent1.DataLayer
+{
+    public interface IUserRepository
+    {
+        //IEnumerable<User> GetALL();
+        //User GetByID(int id);
+        //bool Save(User model);
+        Task<IList<User>> GetAllAsync();
+        Task<User> GetByIdAsyn(int id);
+        Task SaveAsync(User item);
+    }
+}
